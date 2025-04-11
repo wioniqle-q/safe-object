2092be29be7c46cfb0e111605c55cb759ab4684b
Tuna KARA <69215407+wioniqle-q@users.noreply.github.com>
2025-04-11 19:05:31 +0300

    Delete safe-object directory

--- safe-object/safe-object/Interfaces/IVaultService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace safe_object.Interfaces;
-
-public interface IVaultService
-{
-    Task<string> StoreKeyAsync(string fileId, string filePrivateKey, string filePublicMasterKey);
-    Task<string> RetrieveKeyAsync(string fileId, string filePublicMasterKey);
-
-    void Dispose();
-}
